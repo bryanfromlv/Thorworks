b42df4d7a09757dec87702b9de232699d0eb4150
[Bryan Thompson] <bryanfromlv@gmail.com>
2022-01-27 19:23:30 -0800

    Styling contact form

--- contactForm.php
+++ contactForm.php
@@ -1,7 +1,11 @@
 <?php
-if (isset($_POST['submit'])) {
+if (isset($_POST['send'])) {
   // submit button clicked
+  if (!isset($_POST['name']) || !isset($_POST['message'])) {
+    //! error! exit here and report error
+  };
   $name = $_POST['name'];
   $email = $_POST['email'];
   $message = $_POST['message'];
+  $subject = "Message from Thorworks contact form";
 }
